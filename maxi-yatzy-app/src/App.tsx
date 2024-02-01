56ee38c4e9750a182ebabb2233e9a52718fda707
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-02-01 13:47:32 +0100

    feat game setup
    Added game setup, where the player get to chose how they want to play and how many players there is.

--- maxi-yatzy-app/src/App.tsx
+++ maxi-yatzy-app/src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import RulesPopup from "./Components/RulesPopup";
+import GameSetup from "./Components/GameSettup";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [rulesOpen, setRulesOpen] = useState(false);
+
+    const RulesButton = () => {
+        setRulesOpen(!rulesOpen);
+    };
+    return (
+        <div className="App">
+            {rulesOpen ? (
+                <div
+                    style={{
+                        position: "absolute",
+                    }}
+                >
+                    <button
+                        onClick={RulesButton}
+                        style={{
+                            position: "absolute",
+                            marginLeft: "800px",
+
+                            marginTop: "6px",
+                            fontSize: "24px",
+                            borderRadius: "50%",
+                            width: "40px",
+                            height: "40px",
+                            backgroundColor: "lightgrey",
+                        }}
+                    >
+                        X
+                    </button>
+                    <RulesPopup />
+                </div>
+            ) : (
+                <></>
+            )}
+            <div>
+                <h1> Maxi Yatzy</h1>
+                <button
+                    onClick={RulesButton}
+                    style={{
+                        position: "absolute",
+                        marginLeft: "900px",
+                        width: "120px",
+                        height: "60px",
+                        marginTop: "-90px",
+                    }}
+                >
+                    Game Rules
+                </button>
+                <div
+                    style={{
+                        backgroundColor: "whitesmoke",
+                        margin: "25px",
+                        paddingBottom: "20px",
+                        border: "1px solid brown",
+                    }}
+                >
+                    <GameSetup />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
